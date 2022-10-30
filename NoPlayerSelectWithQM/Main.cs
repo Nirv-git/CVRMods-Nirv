1f3593684fd1fdaa0aaae2f836d8fb0d883ae133
a <81605232+Nirv-git@users.noreply.github.com>
2022-10-30 11:38:19 -0500

    NoPlayerSelectWithQM init build

---
+++ NoPlayerSelectWithQM/Main.cs
@@ -0,0 +1,96 @@
+﻿using System.Reflection;
+using MelonLoader;
+using UnityEngine;
+using ABI_RC.Core.InteractionSystem;
+using ABI_RC.Core.Player;
+using HarmonyLib;
+using System;
+using System.Linq;
+using System.Collections;
+
+[assembly: MelonInfo(typeof(NoPlayerSelectWithQM.Main), "NoPlayerSelectWithQM", NoPlayerSelectWithQM.Main.versionStr, "Nirvash")] 
+[assembly: MelonGame(null, "ChilloutVR")]
+
+namespace NoPlayerSelectWithQM
+{
+
+    public class Main : MelonMod
+    {
+        public const string versionStr = "0.0.3";
+        public static MelonLogger.Instance Logger;
+
+        public static bool firstload = true;
+        public static MelonPreferences_Entry<bool> disPlayerSelectQM;
+        public static bool QMstate = false;
+        public static bool LargeMenuState = false;
+
+        public override void OnApplicationStart()
+        {
+            Logger = new MelonLogger.Instance("NoPlayerSelectWithQM");
+
+            MelonPreferences.CreateCategory("NoPlayerSelectWithQM", "NoPlayerSelectWithQM");
+            disPlayerSelectQM = MelonPreferences.CreateEntry<bool>("NoPlayerSelectWithQM", "disPlayerSelectQM", true, "Disable Player Selection Collider when Quick Menu is open");
+            disPlayerSelectQM.OnValueChanged += ReEnableColliders;
+        }
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            switch (buildIndex)//Without switch this would run 3 times at world load
+            {
+                case 0: break;
+                case 1: break;
+                case 2: break;
+                default:
+                    if (firstload)
+                    {
+                        HarmonyInstance.Patch(typeof(CVR_MenuManager).GetMethod(nameof(CVR_MenuManager.ToggleQuickMenu)), null, new HarmonyMethod(typeof(Main).GetMethod(nameof(QMtoggle), BindingFlags.NonPublic | BindingFlags.Static)));
+                        HarmonyInstance.Patch(typeof(ViewManager).GetMethods().First(m => m.Name == nameof(ViewManager.UiStateToggle) && m.GetParameters().Length == 1), null, new HarmonyMethod(typeof(Main).GetMethod(nameof(LargeMenuToggle), BindingFlags.NonPublic | BindingFlags.Static)));
+                        //Logger.Msg("default" + buildIndex);
+                        firstload = false;
+                    }
+                    break;
+            }
+        }
+
+        private static void QMtoggle(bool __0)
+        {
+            //Logger.Msg($"Small Menu {__0}");
+            QMstate = __0;
+            if (LargeMenuState) //Don't disable colliders when large menu is open
+                return;
+            if (disPlayerSelectQM.Value) ToggleColliders(!__0);
+        }
+        private static void LargeMenuToggle(bool __0)
+        {
+            //Logger.Msg($"Large Menu {__0}");
+            LargeMenuState = __0;
+            if (disPlayerSelectQM.Value) ToggleColliders(__0);
+        }
+        private static void ReEnableColliders(bool oldValue, bool newValue)
+        { //If you disable the Melonpref that toggles colliders on and off, make sure colliders are reenabled
+            if(newValue == false)
+                ToggleColliders(true);
+        }
+
+        private static void ToggleColliders(bool setTo)
+        {
+            var players = GameObject.FindObjectsOfType<PlayerDescriptor>();
+            if (players.Length == 0)
+                return;
+
+            for (int i = 0; i < players.Length; i++)
+            {
+                try
+                {
+                    if (players[i].gameObject == PlayerSetup.Instance.gameObject)
+                        continue;
+
+                    players[i].gameObject.GetComponent<CapsuleCollider>().enabled = setTo;
+                }
+                catch (System.Exception ex) { Logger.Msg($"Error for {i} | SetTo {setTo} - QM {QMstate} - LM {LargeMenuState}\n" + ex.ToString()); }
+            }
+        }
+    }
+}
+
+
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+
 **Mods are NOT against CVR's TOS. However be smart when using them, and if you want to submit a bug report to CVR, test with --no-mods first**
 
 **This was made in my free time, and is provided AS IS without warranty of any kind, express or implied. Any use is at your own risk and you accept all responsibility**
@@ -36,12 +37,8 @@ There are a few MelonPrefs that can be customized - I recommend using [UI Expans
 
 
 
-# NamePlateWithQMmod
-Simple mod, this will enable/disable Nameplates when your QuickMenu is open, similar to VRC. 
-There is a MelonPref that lets you disable this behavior.
- 
-'Show Nameplates' in CVR settings must be enabled
-![image](https://user-images.githubusercontent.com/81605232/185835628-ff2e0b0c-cd8a-429e-b58b-7502fb594846.png)
+
+
 # SitLaydownMod
 This mod is meant to reproduce some of the functionality for custom [Sit/Laydown animations that could be added to an avatar in VRC](https://github.com/Dervali-git/VRC-Tips/blob/main/LaySittingPrefab.md) for half body users, however I didn't want to figure out how to do it with avatar animators and am now abusing chairs!  ~~Also this works on all avatars and I don't have to support people configuring it on their own one~~     
 
@@ -66,16 +63,23 @@ There are a few MelonPrefs that can be customized that aren't included on the me
   * Enabled color for QuickMenu items (0=Orange, 1=Yellow, 2=Pink)    
   * Position Offset for settings toggle button - This offsets the toggle button that opens the setting menu, negative moves it further away from the center.     
 
+# NoPlayerSelectWithQM
 
+Simple mod, this will enable/disable other player's selection colliders when only your QuickMenu is open. Comes with a MelonPref to enable/disable the mod. 
+
+# ~~NamePlateWithQMmod~~
+Outdated, built into game now
+
+~~Simple mod, this will enable/disable Nameplates when your QuickMenu is open, similar to VRC. 
+There is a MelonPref that lets you disable this behavior.~~
+~~'Show Nameplates' in CVR settings must be enabled~~
+(https://user-images.githubusercontent.com/81605232/185835628-ff2e0b0c-cd8a-429e-b58b-7502fb594846.png)
 
 # ~~VoiceFalloffAdjMod~~
-
 Outdated 9/17/2022
 
-A simple mod that adds the ability to change the max voice distance for other players in a range from 1-14.5 meters. *The real max is 10meters, I assume as a bandwidth saving feature(?), however I have the slider max out at 14.5 so you can hear someone at that 10meter point)
-
-The UI is the same as PortableMirror and is aligned not to conflict with it. In Melon Prefs you can change the position of the menu. (I recommend using https://github.com/sinai-dev/MelonPreferencesManager | For CVR use the Mono version)   
-  * QuickMenu Position (0=Right, 1=Top, 2=Left) - If the mirror menu is on the Left/Right/Top side of your QuickMenu
-  * Enabled color for QuickMenu items (0=Orange, 1=Yellow, 2=Pink)
-
-(https://user-images.githubusercontent.com/81605232/185814627-5f02b27c-abb6-4e48-ac7e-d26888e1b90e.png)~~~
+~~A simple mod that adds the ability to change the max voice distance for other players in a range from 1-14.5 meters. *The real max is 10meters, I assume as a bandwidth saving feature(?), however I have the slider max out at 14.5 so you can hear someone at that 10meter point)~~
+~~The UI is the same as PortableMirror and is aligned not to conflict with it. In Melon Prefs you can change the position of the menu. (I recommend using https://github.com/sinai-dev/MelonPreferencesManager | For CVR use the Mono version)~~   
+  * ~~QuickMenu Position (0=Right, 1=Top, 2=Left) - If the mirror menu is on the Left/Right/Top side of your QuickMenu~~
+  * ~~Enabled color for QuickMenu items (0=Orange, 1=Yellow, 2=Pink)~~
+(https://user-images.githubusercontent.com/81605232/185814627-5f02b27c-abb6-4e48-ac7e-d26888e1b90e.png)
