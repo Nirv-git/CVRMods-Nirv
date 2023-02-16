55c7e204aac6b5acd5ddccca86c1294baaf100d6
a <81605232+Nirv-git@users.noreply.github.com>
2023-02-16 10:39:57 -0600

    Init Nearclip

---
+++ NearClippingPlaneAdjuster/BTKUI.cs
@@ -0,0 +1,136 @@
+﻿using System.Reflection;
+using UnityEngine;
+using System.IO;
+using BTKUILib;
+using System.Collections.Generic;
+
+namespace NearClipPlaneAdj
+{
+    public class CustomBTKUI
+    {
+        public static void loadAssets()
+        {
+            QuickMenuAPI.PrepareIcon("NearClipPlaneAdj", "0001", Assembly.GetExecutingAssembly().GetManifestResourceStream("NearClipPlaneAdj.Icons.btk.n0001.png"));
+            QuickMenuAPI.PrepareIcon("NearClipPlaneAdj", "001", Assembly.GetExecutingAssembly().GetManifestResourceStream("NearClipPlaneAdj.Icons.btk.n001.png"));
+            QuickMenuAPI.PrepareIcon("NearClipPlaneAdj", "01", Assembly.GetExecutingAssembly().GetManifestResourceStream("NearClipPlaneAdj.Icons.btk.n01.png"));
+            QuickMenuAPI.PrepareIcon("NearClipPlaneAdj", "05", Assembly.GetExecutingAssembly().GetManifestResourceStream("NearClipPlaneAdj.Icons.btk.n05.png"));
+
+        }
+
+        public static void InitUi()
+        {
+            loadAssets();
+
+            var cat = QuickMenuAPI.MiscTabPage.AddCategory("Nearclip Plane Adjust", "NearClipPlaneAdj");
+            
+            var clipList = new float[] {
+                .05f,
+                .01f,
+                .001f,
+                .0001f
+            };
+
+            foreach (var clip in clipList)
+            {
+                var butt = cat.AddButton($"{clip}", clip.ToString().Replace("0.",""), $"Sets Nearclipping plane to {clip}");
+                butt.OnPress += () =>
+                {
+                    Main.ChangeNearClipPlane(clip, true);
+                };
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//using UnityEngine;
+//using System.IO;
+//using ActionMenuApi.Api;
+//using System.Collections.Generic;
+
+//namespace NearClipPlaneAdj
+//{
+//    public class CustomActionMenu
+//    {
+//        public static Texture2D n05, n01, n001, n0001, plane;
+
+//        public static void loadAssets()
+//        {
+//            n05 = LoadEmbeddedImages("n05.png");
+//            n01 = LoadEmbeddedImages("n01.png");
+//            n001 = LoadEmbeddedImages("n001.png");
+//            n0001 = LoadEmbeddedImages("n0001.png");
+//            plane = LoadEmbeddedImages("plane.png");
+//        }
+
+//        private static Texture2D LoadEmbeddedImages(string imageName)
+//        {
+//            try
+//            {
+//                //Load image into Texture
+//                using var assetStream = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("NearClippingPlaneAdjuster.Icons." + imageName);
+//                using var tempStream = new MemoryStream((int)assetStream.Length);
+//                assetStream.CopyTo(tempStream);
+//                var Texture2 = new Texture2D(2, 2);
+//                ImageConversion.LoadImage(Texture2, tempStream.ToArray());
+//                Texture2.wrapMode = TextureWrapMode.Clamp;
+//                Texture2.hideFlags |= HideFlags.DontUnloadUnusedAsset;
+//                return Texture2;
+//            }
+//            catch (System.Exception ex) { NearClipPlaneAdjMod.Logger.Error("Failed to load image from asset bundle: " + imageName + "\n" + ex.ToString()); return null; }
+//        }
+
+
+//        public static void InitUi()
+//        {
+//            loadAssets();
+
+//            if (NearClipPlaneAdjMod.amapi_ModsFolder.Value)
+//                AMUtils.AddToModsFolder("<color=#ff00ff>NearClip Plane Adj</color>", () => AMsubMenu(), plane);
+//            else
+//                VRCActionMenuPage.AddSubMenu(ActionMenuPage.Options, "<color=#ff00ff>NearClip Plane Adj</color>", () => AMsubMenu(), plane);
+//        }
+
+//        private static void AMsubMenu()
+//        {
+//            var clipList = new float[] {
+//                .05f,
+//                .01f,
+//                .001f,
+//                .0001f
+//            };
+
+//            foreach (var clip in clipList)
+//            { 
+//                CustomSubMenu.AddButton(clip.ToString(), () =>
+//                {
+//                    NearClipPlaneAdjMod main = new NearClipPlaneAdjMod(); main.ChangeNearClipPlane(clip, true);
+//                }, StoredIcon(clip));
+//            }
+//        }
+
+
+//        static ref Texture2D StoredIcon(float key)
+//        {
+//            switch (key)
+//            {
+//                case .05f: return ref n05;
+//                case .01f: return ref n01;
+//                case .001f: return ref n001;
+//                case .0001f: return ref n0001;
+//                default: NearClipPlaneAdjMod.Logger.Msg("Something Broke - StoredIcon Switch"); return ref plane;
+//            }
+//        }
+//    }
+//}
---
+++ NearClippingPlaneAdjuster/NearClippingPlaneAdjuster.cs
@@ -0,0 +1,264 @@
+﻿using UIExpansionKit.API;
+using MelonLoader;
+using UnityEngine;
+using ConsoleColor = System.ConsoleColor;
+using System.Linq;
+using System.Net;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using ABI_RC.Core.InteractionSystem;
+using ABI_RC.Core.Player;
+using HarmonyLib;
+using ABI_RC.Core.Savior;
+
+[assembly: MelonInfo(typeof(NearClipPlaneAdj.Main), "NearClipPlaneAdj", NearClipPlaneAdj.Main.versionStr, "Nirvash")]
+[assembly: MelonGame(null, "ChilloutVR")]
+[assembly: MelonOptionalDependencies("BTKUILib")]
+
+namespace NearClipPlaneAdj
+{
+    public class Main : MelonMod
+    {
+        public const string versionStr = "0.5";
+        public static MelonLogger.Instance Logger;
+
+        public static MelonPreferences_Entry<bool> changeClipOnLoad;
+        public static MelonPreferences_Entry<bool> keybindsEnabled;
+        public static MelonPreferences_Entry<bool> smallerDefault;
+        public static MelonPreferences_Entry<bool> raiseNearClip;
+        //public static MelonPreferences_Entry<bool> changeUIcam;
+        public static MelonPreferences_Entry<bool> BTKUILib_en;
+        public static MelonPreferences_Entry<bool> defaultChangeBlackList;
+
+        public static Dictionary<string, System.Tuple<bool, string>> blackList; 
+        public static float oldNearClip;
+        public static float lastSetNearClip;
+        //public static Camera UIcamera;
+
+        public static bool firstload = true;
+
+        public static MelonPreferences_Entry<bool> debug;
+
+        public override void OnApplicationStart()
+        {
+            Logger = new MelonLogger.Instance("NearClipPlaneAdj", ConsoleColor.DarkYellow);
+
+            MelonPreferences.CreateCategory("NearClipAdj", "NearClipPlane Adjuster");
+            changeClipOnLoad = MelonPreferences.CreateEntry<bool>("NearClipAdj", "changeClipOnLoad", true, "Change NearClip on world load");
+            keybindsEnabled = MelonPreferences.CreateEntry<bool>("NearClipAdj", "Keyboard", true, "Keyboard Shortcuts: '[' - 0.0001, ']' - 0.05");
+            smallerDefault = MelonPreferences.CreateEntry<bool>("NearClipAdj", "SmallerDefault", false, "Smaller Default Nearclip on World Change - 0.001 vs 0.01");
+            raiseNearClip = MelonPreferences.CreateEntry<bool>("NearClipAdj", "RaiseOnQuickMenu", false, "If using smaller Default Nearclip (0.001) raise to 0.01 when Quick Menu opens.");
+            BTKUILib_en = MelonPreferences.CreateEntry<bool>("NearClipAdj", "BTKUILib_en", true, "BTKUILib Support (Requires Restart)");
+            defaultChangeBlackList = MelonPreferences.CreateEntry("NearClipAdj", "defaultChangeBlackList", true, "Check a blacklist for worlds to not auto change the NearClip on (Restart Required to Enable)");
+
+            debug = MelonPreferences.CreateEntry<bool>("NearClipAdj", "debug", false, "debug");
+
+            var settings = ExpansionKitApi.GetSettingsCategory("NearClipAdj");
+
+            settings.AddSimpleButton("Set Nearplane to 0.05", (() => ChangeNearClipPlane(.05f, true)));
+            settings.AddSimpleButton("Set Nearplane to 0.01", (() => ChangeNearClipPlane(.01f, true)));
+            settings.AddSimpleButton("Set Nearplane to 0.001", (() => ChangeNearClipPlane(.001f, true)));
+            settings.AddSimpleButton("Set Nearplane to 0.0001", (() => ChangeNearClipPlane(.0001f, true)));
+
+
+            if (MelonHandler.Mods.Any(m => m.Info.Name == "BTKUILib") && BTKUILib_en.Value)
+            {
+                CustomBTKUI.InitUi();
+            }
+            else Logger.Msg("BTKUILib is missing, or setting is toggled off in Mod Settings - Not adding controls to BTKUILib");
+
+            if (defaultChangeBlackList.Value) GetBlackList();
+            MelonCoroutines.Start(SampleNearClip());
+        }
+        private void GetBlackList()
+        {
+            try
+            {
+                blackList = new Dictionary<string, System.Tuple<bool, string>>();
+                string url = "https://raw.githubusercontent.com/Nirv-git/CVRMods-Nirv/main/NearClippingPlaneAdjuster/blacklist";
+                WebClient client = new WebClient();
+                var temp = client.DownloadString(url);
+                using (System.IO.StringReader reader = new System.IO.StringReader(temp))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        var sub = line.Split(',');
+                        if(sub.Length == 3) blackList.Add(sub[0], new System.Tuple<bool, string>(bool.Parse(sub[1]), sub[2]));
+                    }
+                }
+            }
+            catch (Exception ex) { Logger.Error($"GetBlackList error\n" + ex.ToString()); }
+        }
+
+        public override void OnPreferencesSaved()
+        {
+           
+        }
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            Logger.Msg($"name: {sceneName}, index: {buildIndex}");
+            switch (buildIndex)//Without switch this would run 3 times at world load
+            {
+                case 0: //Prep
+                    break;
+                case 1: //Login
+                    break;
+                case 2: //Init
+                    break;
+                case 3: //HQ
+                    break;
+                default:
+
+                    if (firstload)
+                    {
+                        HarmonyInstance.Patch(typeof(CVR_MenuManager).GetMethod(nameof(CVR_MenuManager.ToggleQuickMenu)), null, new HarmonyMethod(typeof(Main).GetMethod(nameof(QMtoggle), BindingFlags.NonPublic | BindingFlags.Static)));
+                        //Logger.Msg("default" + buildIndex);
+                        firstload = false;
+                    }
+                    if (changeClipOnLoad.Value) MelonCoroutines.Start(SetNearClipPlane(0.01f));
+                    break;
+            }
+
+        }
+
+
+        private static void QMtoggle(bool __0)
+        {//Will send false when Big menu opens, check for bugs related
+            if (debug.Value) Logger.Msg($"QMtoggle: {__0}");
+            if (!raiseNearClip.Value) return;
+            if (oldNearClip == 0 ) return;
+
+            var screenCamera = GetScreenCam();
+            if (screenCamera is null) return;
+
+            if (__0)
+            {
+                float clipValue = screenCamera.nearClipPlane;
+                if (clipValue < .0011f && clipValue > .0009f)
+                    ChangeNearClipPlane(.01f, false);
+                oldNearClip = clipValue;
+            }
+            else
+            {
+                float clipValue = screenCamera.nearClipPlane;
+                if (clipValue != oldNearClip)
+                    ChangeNearClipPlane(oldNearClip, false);
+            }
+        }
+
+
+        private static Camera GetScreenCam()
+        {
+            Camera screenCamera = PlayerSetup.Instance.GetActiveCamera().GetComponent<Camera>();
+            return screenCamera;
+        }
+
+        public static void ChangeNearClipPlane(float value, bool printMsg)
+        {
+            var screenCamera = GetScreenCam();
+            if (screenCamera is null) return;
+            float oldvalue = screenCamera.nearClipPlane;
+            screenCamera.nearClipPlane = value;
+            if (printMsg) Logger.Msg($"Nearplane chnaged. Old: {oldvalue}, New: {value} {(keybindsEnabled.Value ? "- Keyboard Hotkeys: '[' - 0.0001, ']' - 0.05" : "")}");
+            //ChangePhotoCameraNearField(value);
+            oldNearClip = screenCamera.nearClipPlane;
+
+            //try
+            //{
+            //    if (changeUIcam.Value)
+            //            UIcamera.nearClipPlane = value;
+            //}
+            //catch (System.Exception ex) { Logger.Error($"Error changing UI Camera nearclip\n" + ex.ToString()); }
+        }
+
+
+       
+        
+        System.Collections.IEnumerator SetNearClipPlane(float znear)
+        {
+            printPlane();
+
+            yield return new WaitForSecondsRealtime(15); //Wait 15 seconds after world load before setting the clipping value. Waiting for the next/first frame does not work
+            if (defaultChangeBlackList.Value && MetaPort.Instance.CurrentWorldId != null && blackList != null)
+            { //Check if world is blacklisted from auto change
+                var worldID = MetaPort.Instance.CurrentWorldId;
+                
+                if (debug.Value)
+                {
+                    Logger.Msg($"ID IS {worldID}");
+                    foreach (var worldEnt in blackList)
+                    {
+                        Logger.Msg($"World {worldEnt.Key}, item1 {worldEnt.Value.Item1} item2 {worldEnt.Value.Item2}");
+                    }
+                }
+                if (blackList.TryGetValue(worldID, out var world))
+                {
+                    if (world.Item1)
+                    {
+                        Logger.Msg(ConsoleColor.Yellow, $"Not auto adjusting NearClipping plane, world blacklisted for 0.01 and lower values. \nDebug-{worldID}, {world.Item2}, {world.Item1}");
+                        yield break;
+                    }
+                    else if(smallerDefault.Value)
+                    {
+                        Logger.Msg(ConsoleColor.Yellow, $"Not auto adjusting NearClipping plane, world blacklisted for 0.001 value. \nDebug-{worldID}, {world.Item2}, {world.Item1}");
+                        yield break;
+                    }
+                }
+            }
+            
+            if (smallerDefault.Value) znear = 0.001f;
+            ChangeNearClipPlane(znear, true);
+            oldNearClip = znear;
+            Logger.Msg("Near plane adjusted after world load");
+        }
+
+
+        public override void OnUpdate()
+        {
+            if (!keybindsEnabled.Value) return;
+
+            if (Input.GetKeyDown(KeyCode.LeftBracket))
+            {
+                Logger.Msg(ConsoleColor.DarkCyan, "NearClip set to smallest value on keypress: 0.0001");
+                ChangeNearClipPlane(.0001f, true);
+            }
+            if (Input.GetKeyDown(KeyCode.RightBracket))
+            {
+                Logger.Msg(ConsoleColor.DarkCyan, "NearClip set to largest value on keypress: 0.05");
+                ChangeNearClipPlane(.05f, true);
+            }
+        }
+
+
+        System.Collections.IEnumerator SampleNearClip()
+        {
+            while (true) 
+            {
+                try
+                {
+                    if (debug.Value)
+                    {
+                        printPlane();
+                    }
+                }
+                catch { }
+                yield return new WaitForSecondsRealtime(2);
+            }
+        }
+
+        public static void printPlane()
+        {
+            var screenCamera = GetScreenCam();
+            if (screenCamera is null) return;
+            float value = screenCamera.nearClipPlane;
+            Logger.Msg($"Near plane cur: {value}");
+
+        }
+
+
+
+    }
+}
