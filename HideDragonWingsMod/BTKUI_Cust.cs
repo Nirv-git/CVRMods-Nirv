40006ac28ca31e945b977c4214e3e0c08da8168a
a <81605232+Nirv-git@users.noreply.github.com>
2023-12-23 22:00:58 -0600

    Updates for BTKUI2

--- HideDragonWingsMod/BTKUI_Cust.cs
+++ HideDragonWingsMod/BTKUI_Cust.cs
@@ -4,6 +4,9 @@ using System.IO;
 using BTKUILib;
 using BTKUILib.UIObjects;
 using System.Collections.Generic;
+using MelonLoader;
+using System.Linq;
+using Semver;
 
 namespace HideDragonWings
 {
@@ -11,28 +14,47 @@ namespace HideDragonWings
     {
         public static void loadAssets()
         {
-            QuickMenuAPI.PrepareIcon("NirvMisc", "NirvMisc", Assembly.GetExecutingAssembly().GetManifestResourceStream("HideDragonWings.Icons.NirvMisc.png"));
-            QuickMenuAPI.PrepareIcon("HideDragonWings", "hdw-Empty", Assembly.GetExecutingAssembly().GetManifestResourceStream("HideDragonWings.Icons.wing-256.png"));
-            QuickMenuAPI.PrepareIcon("HideDragonWings", "hdw-Full", Assembly.GetExecutingAssembly().GetManifestResourceStream("HideDragonWings.Icons.wing-full-256.png"));
+            QuickMenuAPI.PrepareIcon(ModName, "NirvMisc", Assembly.GetExecutingAssembly().GetManifestResourceStream("HideDragonWings.Icons.NirvMisc.png"));
+            QuickMenuAPI.PrepareIcon(ModName, "hdw-Empty", Assembly.GetExecutingAssembly().GetManifestResourceStream("HideDragonWings.Icons.wing-256.png"));
+            QuickMenuAPI.PrepareIcon(ModName, "hdw-Full", Assembly.GetExecutingAssembly().GetManifestResourceStream("HideDragonWings.Icons.wing-full-256.png"));
         }
+
+        public static string ModName = "NirvBTKUI";
+        private static MethodInfo _btkGetCreatePageAdapter;
 
         public static Category mainCat;
         public static Dictionary<GameObject, Vector3> wingDict = new Dictionary<GameObject, Vector3>();
 
         public static void InitUi()
         {
+            if (MelonMod.RegisteredMelons.Any(x => x.Info.Name.Equals("BTKUILib") && x.Info.SemanticVersion.CompareByPrecedence(new SemVersion(1, 9)) > 0))
+            {
+                //We're working with UILib 2.0.0, let's reflect the get create page function
+                _btkGetCreatePageAdapter = typeof(Page).GetMethod("GetOrCreatePage", BindingFlags.Public | BindingFlags.Static);
+                Main.Logger.Msg($"BTKUILib 2.0.0 detected, attempting to grab GetOrCreatePage function: {_btkGetCreatePageAdapter != null}");
+            }
+            if (!Main.useNirvMiscPage.Value)
+            {
+                ModName = "hideDragonWingsMod";
+            }
+
             loadAssets();
             Category cat = null;
             if (Main.useNirvMiscPage.Value)
             {
-                var page = new Page("NirvMisc", "Nirv Misc Page", true, "NirvMisc");
+                //var page = new Page("NirvMisc", "Nirv Misc Page", true, "NirvMisc");
+                Page page = null;
+                if (_btkGetCreatePageAdapter != null)
+                    page = (Page)_btkGetCreatePageAdapter.Invoke(null, new object[] { ModName, "Nirv Misc Page", true, "NirvMisc", null, false });
+                else
+                    page = new Page(ModName, "Nirv Misc Page", true, "NirvMisc");
                 page.MenuTitle = "Nirv Misc Page";
                 page.MenuSubtitle = "Misc page for mods by Nirv, can disable this in MelonPrefs for the individual mods";
-                cat = page.AddCategory("Hide Dragon Wings", "HideDragonWings");
+                cat = page.AddCategory("Hide Dragon Wings");
             }
             else
             {
-                cat = QuickMenuAPI.MiscTabPage.AddCategory("Hide Dragon Wings", "HideDragonWings");
+                cat = QuickMenuAPI.MiscTabPage.AddCategory("Hide Dragon Wings", ModName);
             }
             mainCat = cat;
             PopulateButtons();
