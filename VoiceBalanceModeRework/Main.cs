3db4ffc883fb7f87a6d8bda1a16d08da72ca9efd
a <81605232+Nirv-git@users.noreply.github.com>
2024-01-23 21:01:57 -0600

    VoiceBalanceModeRework
    Init version

---
+++ VoiceBalanceModeRework/Main.cs
@@ -0,0 +1,235 @@
+﻿//This is free and unencumbered software released into the public domain.
+//
+//Anyone is free to copy, modify, publish, use, compile, sell, or
+//distribute this software, either in source code form or as a compiled
+//binary, for any purpose, commercial or non-commercial, and by any
+//means.
+//
+//In jurisdictions that recognize copyright laws, the author or authors
+//of this software dedicate any and all copyright interest in the
+//software to the public domain. We make this dedication for the benefit
+//of the public at large and to the detriment of our heirs and
+//successors. We intend this dedication to be an overt act of
+//relinquishment in perpetuity of all present and future rights to this
+//software under copyright law.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+//MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+//IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+//OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+//ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+//OTHER DEALINGS IN THE SOFTWARE.
+//
+//For more information, please refer to <https://unlicense.org>
+
+
+
+
+using MelonLoader;
+using UnityEngine;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections;
+using System.Reflection;
+using ABI_RC.Core.Player;
+using HarmonyLib;
+using ABI_RC.Systems.Audio;
+
+[assembly: MelonGame(null, "ChilloutVR")]
+[assembly: MelonInfo(typeof(VoiceBalanceModeRework.Main), "VoiceBalanceModeRework", VoiceBalanceModeRework.Main.versionStr, "Nirvash")]
+[assembly: AssemblyVersion(VoiceBalanceModeRework.Main.versionStr)]
+[assembly: AssemblyFileVersion(VoiceBalanceModeRework.Main.versionStr)]
+[assembly: MelonColor(ConsoleColor.DarkCyan)]
+
+namespace VoiceBalanceModeRework
+{
+    public class Main : MelonMod
+    {
+        public static MelonLogger.Instance Logger;
+        public const string versionStr = "0.0.1";
+
+        public static MelonPreferences_Category cat;
+        private const string catagory = "VoiceBalanceModeRework";
+        public static MelonPreferences_Entry<bool> modEnabled;
+        public static MelonPreferences_Entry<bool> nearbyFocus;
+        public static MelonPreferences_Entry<bool> leftFocus;
+        public static MelonPreferences_Entry<bool> rightFocus;
+        public static MelonPreferences_Entry<int> earFocusAngle;
+        public static MelonPreferences_Entry<int> minVol;
+        public static MelonPreferences_Entry<bool> debug;
+        public static MelonPreferences_Entry<bool> debugStats;
+
+        public override void OnApplicationStart()
+        {
+            Logger = new MelonLogger.Instance("VoiceBalanceModeRework", ConsoleColor.DarkCyan);
+
+            cat = MelonPreferences.CreateCategory(catagory, "VoiceBalanceModeRework");
+            modEnabled = MelonPreferences.CreateEntry(catagory, nameof(modEnabled), true, "Mod Enabled");
+            nearbyFocus = MelonPreferences.CreateEntry(catagory, nameof(nearbyFocus), true, "nearbyFocus");
+            leftFocus = MelonPreferences.CreateEntry(catagory, nameof(leftFocus), false, "leftFocus");
+            rightFocus = MelonPreferences.CreateEntry(catagory, nameof(rightFocus), false, "rightFocus");
+            earFocusAngle = MelonPreferences.CreateEntry(catagory, nameof(earFocusAngle), 60, "earFocusAngle");
+            minVol = MelonPreferences.CreateEntry(catagory, nameof(minVol), 30, "Min Volume");
+            debug = MelonPreferences.CreateEntry(catagory, nameof(debug), false, "Debug log spam");
+            debugStats = MelonPreferences.CreateEntry(catagory, nameof(debugStats), false, "Debug profiling");
+        }
+    }
+
+    [HarmonyPatch]
+    internal class HarmonyPatches
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(CVRFocusAttenuation), nameof(CVRFocusAttenuation.Apply))]
+        internal static bool OnApply(ref float __result,
+                    CVRFocusAttenuation.eFocusMode mode, bool leftEarFocus, bool rightEarFocus, float multiplierMin, float multiplierMax, float volume,
+                    Vector3 speakerPosition, Vector3 speakerForward, Vector3 listenerPosition, Vector3 listenerForward, Vector3 listenerRight)
+        { 
+            if (!Main.modEnabled.Value)
+                return true;
+
+            if (Main.debug.Value) Main.Logger.Msg($"mode: {mode} multiplierMin: {multiplierMin} multiplierMax: {multiplierMax} volume: {volume} Left:{leftEarFocus} Right:{rightEarFocus}");
+
+            if (mode == CVRFocusAttenuation.eFocusMode.None)
+            {
+                __result = volume;
+                return false;
+            }
+
+            var listenerAngle = Vector3.Angle(listenerForward, speakerPosition - listenerPosition);
+            if (Main.debug.Value) Main.Logger.Msg("listenerAngle: " + listenerAngle);
+            var speakerAngle = Vector3.Angle(speakerForward, listenerPosition -  speakerPosition);
+            if (Main.debug.Value) Main.Logger.Msg("speakerAngle: " + speakerAngle);
+
+            //https://www.dpamicrophones.com/mic-university/facts-about-speech-intelligibility
+            //linear fit {45,1},{90,.8},{120,.66},{150,.5}
+            //1.21944 - 0.0047352 x
+            var speakerVolume = 0f;
+            if (speakerAngle <= 45f)
+                speakerVolume = 1f;
+            else if (speakerAngle >= 150f)
+                speakerVolume = .5f;
+            else
+                speakerVolume = 1.21944f - (0.0047352f * speakerAngle);
+            if (Main.debug.Value) Main.Logger.Msg(ConsoleColor.DarkCyan, "speakerVolume: " + speakerVolume);
+
+
+            var listenerVolume = 0f;
+            switch (mode)
+            {
+                case CVRFocusAttenuation.eFocusMode.Balanced:
+                    //quadratic fit {45,1},{90,.75},{180,.5}
+                    //0.0000205761 x^2 - 0.00833333 x + 1.33333
+                    if (listenerAngle <= 45f)
+                        listenerVolume = 1f;
+                    else
+                        listenerVolume = (0.0000205761f * Mathf.Pow(listenerAngle, 2)) - (0.00833333f * listenerAngle) + 1.33333f;
+                    break;
+                case CVRFocusAttenuation.eFocusMode.Forward:
+                    //quadratic fit {45,1},{90,.66},{135,.33},{180,.2}
+                    //0.0000259259 x^2 - 0.0119 x + 1.4925
+                    if (listenerAngle <= 45f)
+                        listenerVolume = 1f;
+                    else
+                        listenerVolume = (0.0000259259f * Mathf.Pow(listenerAngle, 2f)) - (0.0119f * listenerAngle) + 1.4925f;
+                    break;
+                case CVRFocusAttenuation.eFocusMode.Backward:
+                    //quadratic fit {0,.25},{45,.33},{90,.66},{135,1}
+                    //0.0000320988 x^2 + 0.0014 x + 0.238
+                    if (listenerAngle >= 135f)
+                        listenerVolume = 1f;
+                    else
+                        listenerVolume = (0.0000320988f * Mathf.Pow(listenerAngle, 2)) + (0.0014f * listenerAngle) + 0.238f;
+                    break;
+            }
+            if (Main.debug.Value) Main.Logger.Msg(ConsoleColor.Cyan, "listenerVolume: " + listenerVolume);
+            
+
+            var earAngle = Main.earFocusAngle.Value;
+            var speakerEarAngle = Vector3.Angle(listenerRight, listenerPosition - speakerPosition); //Left = 0 | Right = 180 
+            var earFocusVol = 0f;
+            if (Main.leftFocus.Value)//leftEarFocus) Current broke due to game settings bug
+            {
+                if (Main.debug.Value) Main.Logger.Msg(ConsoleColor.Green, "speakerEarAngleLeft: " + speakerEarAngle);       
+                var speakerEarAngleLeft = Mathf.Clamp(speakerEarAngle, 0f, earAngle); // Ensure the value is within range
+                var speakerEarLeftVolume = 1f - speakerEarAngleLeft / earAngle; // Normalize the value to the range 0 to 1 with 0 being 1
+                if (Main.debug.Value) Main.Logger.Msg(ConsoleColor.Green, "speakerEarLeftVolume: " + speakerEarLeftVolume);
+                earFocusVol += speakerEarLeftVolume;
+            }
+            if (Main.rightFocus.Value)//rightEarFocus)
+            {
+                var speakerEarAngleRight = Math.Abs(Mathf.Clamp(speakerEarAngle, (180f - earAngle), 180f) - 180);
+                if (Main.debug.Value) Main.Logger.Msg(ConsoleColor.DarkGreen, "speakerEarAngleRight: " + speakerEarAngleRight);
+                var speakerEarRightVolume = 1f - speakerEarAngleRight / earAngle;
+                if (Main.debug.Value) Main.Logger.Msg(ConsoleColor.DarkGreen, "speakerEarRightVolume: " + speakerEarRightVolume);
+                earFocusVol += speakerEarRightVolume;
+            }
+
+
+            // Distanced based audio boost
+            var distBoost = 0F;
+            if (Main.nearbyFocus.Value)
+            {
+                var height = PlayerSetup.Instance.GetAvatarHeight();
+                var dist = Vector3.Distance(speakerPosition, listenerPosition);
+                if (dist < height) 
+                {
+                    var scale = dist / height; 
+                    //input, output
+                    //quadratic fit {1,0},{.6,.35},{.2,1}
+                    //0.9375 x^2 - 2.375 x + 1.4375
+                    if (scale < 0.2f)
+                        distBoost = 1f;
+                    else
+                        distBoost = Mathf.Clamp(((0.9375f * Mathf.Pow(scale, 2)) - (2.375f * scale) + 1.4375f), 0f, 1f);
+                    if (Main.debug.Value) Main.Logger.Msg(ConsoleColor.DarkMagenta, $"distBoost: {distBoost:F2}| height: {height:F2} dist{dist:F2} scale{scale:F2}");
+                }
+            }
+
+
+            var endVolume = (speakerVolume * listenerVolume);
+            if (Main.debug.Value) Main.Logger.Msg(ConsoleColor.Yellow, "endVolume-Raw: " + endVolume);
+
+            var endVolumeAdj = endVolume + .75f * earFocusVol;
+            if (Main.debug.Value) Main.Logger.Msg(ConsoleColor.DarkYellow, "endVolume-Ear: " + endVolumeAdj);
+
+            endVolumeAdj += .75f * distBoost;
+            if (Main.debug.Value) Main.Logger.Msg(ConsoleColor.Yellow, "endVolume-Dist: " + endVolumeAdj);
+
+            endVolumeAdj = Mathf.Clamp(endVolumeAdj, Math.Max(multiplierMin, Main.minVol.Value/100f), multiplierMax);
+
+            __result = volume * endVolumeAdj;
+
+            if (Main.debugStats.Value) Main.Logger.Msg($"listenerAngle: {listenerAngle:F0} Volume: {__result:F2}");
+            if (Main.debugStats.Value) Main.Logger.Msg($"speakerAngle: {speakerAngle:F0} Volume: {speakerVolume:F2}");
+
+            return false;
+        }
+
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(CVRFocusAttenuation), nameof(CVRFocusAttenuation.Apply))]
+        internal static void OnApplyPost(ref float __result,
+                    CVRFocusAttenuation.eFocusMode mode, bool leftEarFocus, bool rightEarFocus, float multiplierMin, float multiplierMax, float volume,
+                    Vector3 speakerPosition, Vector3 speakerForward, Vector3 listenerPosition, Vector3 listenerForward, Vector3 listenerRight)
+        {
+            if (Main.modEnabled.Value)
+                return;
+
+            if (Main.debug.Value) Main.Logger.Msg($"Volume: {__result}");
+
+            var listenerAngle = Vector3.Angle(listenerForward, speakerPosition - listenerPosition);
+            if (Main.debugStats.Value) Main.Logger.Msg($"listenerAngle: {listenerAngle:F0} Volume: {__result:F2}");
+
+        }  
+    }
+}
+
+
+
+
+
+
+
+
---
+++ VoiceBalanceModeRework/README.md
@@ -0,0 +1,58 @@
+
+# VoiceBalanceModeRework    
+
+### Mod details   
+This mod changes the `Player Voice Attenuation` > `Focus Modes` with the goal of providing a more natural and comfortable sound profile.      
+
+### Settings    
+This mod uses the normal `Settings > Focus Mode` drop down for switching states            
+*Left/Right Ear Focus on this menu are not used due to a game bug (Which has been reported)*         
+![image](https://github.com/Nirv-git/CVRMods-Nirv/assets/81605232/ee4d8491-043c-49c0-ba3b-2554f58e9d12)   
+     
+Additional options are available in the UIX Mod Settings:      
+| Setting | Desc |
+|--|--|
+| ModEnabled | Enables/Disables the Mod |
+| nearbyFocus| Boosts the volume of nearby players based on a range of your avatar height|
+| leftFocus| Boosts the volume of players to your left|
+| rightFocus| Boosts the volume of players to your right|
+| earFocusAngle| Angle of the cone for left/right Focus modes |
+| Min Volume| Lowest volume the mod will attenuate voices to|
+| Debug log spam| Debug that will spam your console with angles and volumes|
+| Debug profiling| Debug that will spam your console with angles and volumes|
+
+![image](https://github.com/Nirv-git/CVRMods-Nirv/assets/81605232/da5ea087-10be-48ae-b3ac-88f1a992efaf)
+
+
+## Logic and Feedback
+This is feedback for ChilloutVR Devs on Focus attenuation; they are free to take any ideas, concepts, straight code from the mod and incorporated it into the game. This is just my attempt at giving real world examples on how this system can be improved.
+`Code for this mod, VoiceBalanceModeRework, is licensed under the Unlicense license and is free to use without restriction`
+
+### Existing 
+The current focus curves result in some uncomfortable audio effects
+* Limited forward zone where players are at full volume  
+*  Sharp edges where voice attention increases drastically   
+*  Odd volumes if you are talking to a player right next to you.    
+
+Balanced without Ear Focus makes it very hard to hear someone to your left/right, even if they are close by, and which makes the Ear Focus modes nearly required. However even with Ear Focus on you have an awkward notch where voices get quieter as you start to turn off the forward axis, then suddenly get louder when your ear is directly facing them. 
+
+![image](https://github.com/Nirv-git/CVRMods-Nirv/assets/81605232/03833234-fd06-42c8-873d-87cd524a6db7)*0 is directly in front of the player and 180 is behind them*
+
+### Rework
+In this re-implementation of `CVRFocusAttenuation.Apply` I started with applying an attenuation curve off of two things,     
+ - Direction of the Speaker toward the Listener     
+ - Direction of the Listener toward the Speaker    
+ 
+The logic behind this is that [someone's voice will be louder if they are facing you](https://www.dpamicrophones.com/mic-university/facts-about-speech-intelligibility), however if you are turned away from them you may not want to hear them as at full volume. These two values are multiplied together to get our raw attenuation value. 
+
+The curves used for each of these modes are somewhat arbitrary, however were selected to try and provide a gradual audio change across the range. For Balanced and Forward there is a 90° cone where no attenuation is applied as generally those in your FOV you should expect to hear normally. The Ear Focus modes use a 60° cone by default to overlap with the forward section.
+
+![image](https://github.com/Nirv-git/CVRMods-Nirv/assets/81605232/b2e1159e-5ba1-4b08-9931-34272b23fe98)*0 is directly in front of the player and 180 is behind them*
+
+
+In addition to this I added in a 'Nearby Focus' mode, what this does is boost the volume of nearby players, based on your avatar height. Logic behind this is that you should always hear other players that are right up next to you. This curve caps out when the other player is 30% of your height from your head. 
+
+![image](https://github.com/Nirv-git/CVRMods-Nirv/assets/81605232/a6b59874-3d0a-42aa-b520-417e7549f2bd)
+I would honestly drop Ear Focus mode and just keep Nearby Boost as it likely fills the same use case for many. 
+
+**If you have any questions, or feedback please feel free to reach out to me.** 
