b67466470461cd6a2b362115f5a34e97539adcfd
a <81605232+Nirv-git@users.noreply.github.com>
2024-02-06 17:04:36 -0600

    FlightBinding Init Version

---
+++ FlightBinding/Main.cs
@@ -0,0 +1,47 @@
+﻿using MelonLoader;
+using UnityEngine;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections;
+using System.Reflection;
+using VRBinding;
+using Valve.VR;
+using ABI_RC.Systems.Movement;
+
+[assembly: MelonGame(null, "ChilloutVR")]
+[assembly: MelonInfo(typeof(FlightBinding.Main), "FlightBinding", FlightBinding.Main.versionStr, "Nirvash")]
+[assembly: AssemblyVersion(FlightBinding.Main.versionStr)]
+[assembly: AssemblyFileVersion(FlightBinding.Main.versionStr)]
+[assembly: MelonColor(ConsoleColor.DarkBlue)]
+
+namespace FlightBinding
+{
+    public class Main : MelonMod
+    {
+        public static MelonLogger.Instance Logger;
+        public const string versionStr = "0.0.1";
+
+        public override void OnInitializeMelon()
+        {
+            Logger = new MelonLogger.Instance("FlightBinding", ConsoleColor.DarkBlue);
+
+            VRBindingMod.RegisterBinding("FlightBinding", "Toggle Flight", VRBindingMod.Requirement.optional, a =>
+            {
+                if (a.GetStateDown(SteamVR_Input_Sources.Any) == true)
+                {
+                    Logger.Msg(ConsoleColor.Blue, "Toggling Flight");
+                    BetterBetterCharacterController.Instance.ToggleFlight();
+                }
+            }); 
+        }
+    }
+}
+
+
+
+
+
+
+
+
---
+++ FlightBinding/README.md
@@ -0,0 +1,8 @@
+# FlightBinding
+
+A very simple mod, adds a SteamVR binding for toggling flight on/off. You will need to go into your bindings in SteamVR and assign it.                
+
+**[Requires VRBinding](https://api.cvrmg.com/v1/mods/download/174)**               
+   
+![image](https://github.com/Nirv-git/CVRMods-Nirv/assets/81605232/0ba9c376-101b-4528-a588-0479976b7176)                  
+![image](https://github.com/Nirv-git/CVRMods-Nirv/assets/81605232/244f20e1-5640-4c7b-b16c-5849cf0a4d26)                    
